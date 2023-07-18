5873aeae63b939c01745a0da7e74c9323316c8ef
callebtc <93376500+callebtc@users.noreply.github.com>
2023-07-18 22:20:03 +0200

    update to 337456333eaaea41e3d03633188ed4379d723954

--- lib/cashu/core/settings.py
+++ lib/cashu/core/settings.py
@@ -8,7 +8,7 @@ from pydantic import BaseSettings, Extra, Field
 
 env = Env()
 
-VERSION = "0.12.2"
+VERSION = "0.12.3"
 
 
 def find_env_file():
@@ -98,7 +98,7 @@ class WalletSettings(CashuSettings):
         ]
     )
 
-    timelock_delta_seconds: int = Field(default=86400)  # 1 day
+    locktime_delta_seconds: int = Field(default=86400)  # 1 day
 
 
 class Settings(
