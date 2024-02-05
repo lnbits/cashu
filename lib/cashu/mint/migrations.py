75065694fd6485b00671676af87fe2d961b16aca
callebtc <93376500+callebtc@users.noreply.github.com>
2024-02-05 18:08:35 +0100

    sync to nusthell

--- lib/cashu/mint/migrations.py
+++ lib/cashu/mint/migrations.py
@@ -312,8 +312,9 @@ async def m011_add_quote_tables(db: Database):
         await conn.execute(
             f"INSERT INTO {table_with_schema(db, 'mint_quotes')} (quote, method,"
             " request, checking_id, unit, amount, paid, issued, created_time,"
-            " paid_time) SELECT id, 'bolt11', bolt11, payment_hash, 'sat', amount,"
-            f" False, issued, created, NULL FROM {table_with_schema(db, 'invoices')} "
+            " paid_time) SELECT id, 'bolt11', bolt11, COALESCE(payment_hash, 'None'),"
+            f" 'sat', amount, False, issued, COALESCE(created, '{timestamp_now(db)}'),"
+            f" NULL FROM {table_with_schema(db, 'invoices')} "
         )
 
         # drop table invoices
@@ -338,9 +339,9 @@ async def m012_keysets_uniqueness_with_seed(db: Database):
                     id TEXT NOT NULL,
                     derivation_path TEXT,
                     seed TEXT,
-                    valid_from TIMESTAMP NOT NULL DEFAULT {db.timestamp_now},
-                    valid_to TIMESTAMP NOT NULL DEFAULT {db.timestamp_now},
-                    first_seen TIMESTAMP NOT NULL DEFAULT {db.timestamp_now},
+                    valid_from TIMESTAMP,
+                    valid_to TIMESTAMP,
+                    first_seen TIMESTAMP,
                     active BOOL DEFAULT TRUE,
                     version TEXT,
                     unit TEXT,
@@ -357,3 +358,53 @@ async def m012_keysets_uniqueness_with_seed(db: Database):
             f" unit FROM {table_with_schema(db, 'keysets_old')}"
         )
         await conn.execute(f"DROP TABLE {table_with_schema(db, 'keysets_old')}")
+
+
+async def m013_keysets_add_encrypted_seed(db: Database):
+    async with db.connect() as conn:
+        # set keysets table unique constraint to id
+        # copy table keysets to keysets_old, create a new table keysets
+        # with the same columns but with a unique constraint on id
+        # and copy the data from keysets_old to keysets, then drop keysets_old
+        await conn.execute(
+            f"DROP TABLE IF EXISTS {table_with_schema(db, 'keysets_old')}"
+        )
+        await conn.execute(
+            f"CREATE TABLE {table_with_schema(db, 'keysets_old')} AS"
+            f" SELECT * FROM {table_with_schema(db, 'keysets')}"
+        )
+        await conn.execute(f"DROP TABLE {table_with_schema(db, 'keysets')}")
+        await conn.execute(f"""
+                CREATE TABLE IF NOT EXISTS {table_with_schema(db, 'keysets')} (
+                    id TEXT NOT NULL,
+                    derivation_path TEXT,
+                    seed TEXT,
+                    valid_from TIMESTAMP,
+                    valid_to TIMESTAMP,
+                    first_seen TIMESTAMP,
+                    active BOOL DEFAULT TRUE,
+                    version TEXT,
+                    unit TEXT,
+
+                    UNIQUE (id)
+
+                );
+            """)
+        await conn.execute(
+            f"INSERT INTO {table_with_schema(db, 'keysets')} (id,"
+            " derivation_path, valid_from, valid_to, first_seen,"
+            " active, version, seed, unit) SELECT id, derivation_path,"
+            " valid_from, valid_to, first_seen, active, version, seed,"
+            f" unit FROM {table_with_schema(db, 'keysets_old')}"
+        )
+        await conn.execute(f"DROP TABLE {table_with_schema(db, 'keysets_old')}")
+
+        # add columns encrypted_seed and seed_encryption_method to keysets
+        await conn.execute(
+            f"ALTER TABLE {table_with_schema(db, 'keysets')} ADD COLUMN encrypted_seed"
+            " TEXT"
+        )
+        await conn.execute(
+            f"ALTER TABLE {table_with_schema(db, 'keysets')} ADD COLUMN"
+            " seed_encryption_method TEXT"
+        )
