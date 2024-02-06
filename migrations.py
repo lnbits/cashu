4b420baedb66ebc4279f1974de86535eeda269c5
callebtc <93376500+callebtc@users.noreply.github.com>
2024-02-06 16:35:54 +0100

    do not drop columns (#57)

--- migrations.py
+++ migrations.py
@@ -59,8 +59,9 @@ async def m002_add_mint_settings(db):
     # drop cashu.pegs
     await db.execute("DROP TABLE cashu.pegs;")
     # clean up unused columns in cashu.cashu
-    await db.execute("ALTER TABLE cashu.cashu DROP COLUMN issued_sat;")
-    await db.execute("ALTER TABLE cashu.cashu DROP COLUMN coins;")
-    await db.execute("ALTER TABLE cashu.cashu DROP COLUMN tickershort;")
-    await db.execute("ALTER TABLE cashu.cashu DROP COLUMN fraction;")
-    await db.execute("ALTER TABLE cashu.cashu DROP COLUMN maxsats;")
+    # Does not work for some SQLite versions
+    # await db.execute("ALTER TABLE cashu.cashu DROP COLUMN issued_sat;")
+    # await db.execute("ALTER TABLE cashu.cashu DROP COLUMN coins;")
+    # await db.execute("ALTER TABLE cashu.cashu DROP COLUMN tickershort;")
+    # await db.execute("ALTER TABLE cashu.cashu DROP COLUMN fraction;")
+    # await db.execute("ALTER TABLE cashu.cashu DROP COLUMN maxsats;")
